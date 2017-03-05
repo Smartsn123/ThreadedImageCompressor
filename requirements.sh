6b0b8046f060e67928164b11cbbc1fa97cad01e8
Sunny <sunny@Sunnys-MacBook-Air.local>
2017-03-05 12:55:01 +0530

    added requirement file

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install python-pip
+pip install flask
+pip install Pillow
+pip install boto
+pip install requests
+pip install urllib2
+pip instal cStringIO
