40779a73815603c1a15d522695f33bf977766261
Smartsn123 <smartsn123@gmail.com>
2017-03-07 22:43:32 +0530

    feedback updates

--- server.py
+++ server.py
@@ -169,6 +169,6 @@ def send_js(path):
 	
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1',port=8080,threaded=True)
+    app.run(debug=True, host='0.0.0.1',port=8090,threaded=True)
 
     
