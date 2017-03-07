f0a8802369ec073bcd9f70c8d410a0d30f9df556
Smartsn123 <smartsn123@gmail.com>
2017-03-07 23:13:15 +0530

    changes

--- server.py
+++ server.py
@@ -175,6 +175,6 @@ def send_files(path):
 	
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1',port=8090,threaded=True)
+    app.run(debug=True, host='0.0.0.0',port=8090,threaded=True)
 
     
